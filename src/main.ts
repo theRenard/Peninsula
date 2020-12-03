26b49d57c8b1f2f99682597bc2296f25763f1ae8
Daniele <daniele.tabanella@stanley-robotics.com>
2020-12-04 00:15:03 +0100

    set Canvas

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log('ok');
+
+// Get canvas element
+const canvas = document.getElementById("GLCanvas") as HTMLCanvasElement;
+
+// Get webgl context from canvas
+const gl = canvas.getContext("webgl") as WebGLRenderingContext;
+
+
+gl.clearColor(0.0, 0.8, 0.0, 1.0);  // set the color to be cleared
+gl.clear(gl.COLOR_BUFFER_BIT);   // clear the colors
